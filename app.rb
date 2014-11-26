011633b9f8b0f1c79cd52fec8c414079143c4565
M. David <mdavidgreen@gmail.com>
2014-11-25 17:48:16 -0800

    add normalize, set compressed scss style

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 
+configure do
+  set :scss, {:style => :compressed, :debug_info => false}
+end
+
 get '/css/:name.css' do |name|
   content_type :css
   scss "sass/#{name}".to_sym, :layout => false
--- config.ru
+++ config.ru
@@ -1,4 +1,6 @@
 require 'sass/plugin/rack'
-
 require './app'
+
+use Sass::Plugin::Rack
+
 run Sinatra::Application
